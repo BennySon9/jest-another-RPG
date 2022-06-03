3ca574ab89b9cdfb94be41a2c0ac3fe18263a603
Brandon Benson <brandonbenson09@gmail.com>
2022-06-02 20:43:26 -0400

    added getinventory and getstats

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -1,7 +1,6 @@
 const { JestHook } = require("jest-watcher");
 const Player = require("../lib/Player");
 const Potion = require("../lib/Potion");
-console.log(new Potion());
 
 jest.mock("../lib/Potion");
 
@@ -15,4 +14,23 @@ test("creates a player object", () => {
   expect(player.inventory).toEqual(
     expect.arrayContaining([expect.any(Object)])
   );
+});
+
+test("gets player's stats as an object", () => {
+  const player = new Player("Dave");
+
+  expect(player.getStats()).toHaveProperty("potions");
+  expect(player.getStats()).toHaveProperty("health");
+  expect(player.getStats()).toHaveProperty("strength");
+  expect(player.getStats()).toHaveProperty("agility");
+});
+
+test("gets inventory from playert or returns false", () => {
+  const player = new Player("Dave");
+
+  expect(player.getInventory()).toEqual(expect.any(Array));
+
+  player.inventory = [];
+
+  expect(player.getInventory()).toEqual(false);
 });
